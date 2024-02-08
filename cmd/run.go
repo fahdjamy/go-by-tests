dd9dc4632ddc12a52832f17d5f8bf5f819f725b4
fahdjamy <fahdjamy2@gmail.com>
2024-02-07 19:16:50 -0600

    02/07/2024: Server runner test

--- cmd/run.go
+++ cmd/run.go
@@ -1,1 +1,17 @@
-package cmd
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(RunnerMsg())
+}
+
+const RunningPrefix = "running..."
+
+func RunnerMsg() string {
+	return RunningPrefix
+}
+
+func RunnerWithPort(port string) string {
+	return RunningPrefix + " on port, " + port
+}
--- cmd/runner_test.go
+++ cmd/runner_test.go
@@ -1,1 +1,21 @@
 package main
+
+import "testing"
+
+func TestRunnerMsg(t *testing.T) {
+	received := RunnerMsg()
+	expected := RunningPrefix
+
+	if received != expected {
+		t.Errorf("Recieved %q, Expected %q", received, expected)
+	}
+}
+
+func TestRunningWithPort(t *testing.T) {
+	got := RunnerWithPort(":8000")
+	wanted := RunningPrefix + " on port, :8000"
+
+	if got != wanted {
+		t.Errorf("GOT %q, WANTED %q", got, wanted)
+	}
+}
